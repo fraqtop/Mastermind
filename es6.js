f316ea473664cbc4902cc8af29dd0901f2a8974d
fraqtop <fraqtop@gmail.com>
2017-01-27 23:39:51 +0300

    index looks ready

--- es6.js
+++ es6.js
@@ -7,16 +7,5 @@ let {name, age} = obj
 let str =  `Hello, my name is ${name}`
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    generate_gif()
+    
 });
-
-function generate_gif()
-{
-    let black_area = document.querySelector(".represent")
-    let gif = document.createElement("img")
-    gif.src = "circle.gif"
-    for (let i=0; i<10; i++)
-    {
-        black_area.appendChild(gif)
-    }
-}
