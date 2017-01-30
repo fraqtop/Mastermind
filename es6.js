35ac56be54b70f39c8059a0ec5dd3a8a784f6484
fraqtop <abrebib@mail.ru>
2017-01-30 16:50:37 +0300

    generates combination

--- es6.js
+++ es6.js
@@ -1,11 +1,35 @@
-let myfunc = (x,y,z) =>  x+y+z
+const colors = ['green', 'yellow', 'red', 'orange', 'purple', 'blue', 'brown', 'black']
 
-let data = [6, 7, 8]
-let DATA = [10, 10, 10]
-let obj = {"name": "Johnny", "age": 23}
-let {name, age} = obj
-let str =  `Hello, my name is ${name}`
+class Mastermind
+{
+    gen_combination(){
+        if (this.mode=='hard')
+        {
+            for (let i=0; i<4; i++)
+            {
+                this.combs.push(this.cols[this.getRandom()])
+            } 
+        }
+    }
+    getRandom(){
+        let min = 0
+        let max = this.cols.length-1
+        return Math.floor(Math.random()*(max - min +1) + min)
+    }
+    constructor(new_mode)
+    {
+        this.mode = new_mode
+        this.cols = colors
+        this.combs = []
+        if (new_mode=='hard') {this.cols.push('cadetblue')}
+        this.gen_combination()
+    }
+}
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    
+    let mode = location.href.search('mode=')
+    if (mode!=-1)
+    {
+        window.mastermind = new Mastermind(location.href.substr(mode+5))
+    }
 });
